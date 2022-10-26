0ac08deeba46067e9f083eb456c9131afb7294da
christina <christinamharris83@gmail.com>
2022-10-26 08:39:14 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,208 @@
+const Data = require('./data');
+// Require necessary node modules
+// Make the variables inside the .env element available to our Node project
+require('dotenv').config();
+
+const vips = [
+    "angrytxicchaobla",
+    "canniman777",
+    "definatelynotelsie",
+    "deuzex85",
+    "freeside11",
+    "gangmediator",
+    "guccigottheflu",
+    "johnharris85",
+    "love_ivy54",
+    "t7g_",
+    "thecriticalchance",
+    "thegiiil",
+    "tougodotio",
+    "rock_a_goth",
+    "kamikifoxy92"
+
+]
+
+const tmi = require('tmi.js');
+
+
+//timers
+
+function tyTimer() {
+    let ty = "Thank you so much for stopping by and hanging out! 🎉"
+    client.say("#rock_a_goth", ty);
+}
+
+function followTimer() {
+    let follow = "✨ Please FOLLOW to show your support! ✨ Thank You! 🤗"
+    client.say("#rock_a_goth", follow);
+}
+
+function rulesTimer() {
+    let rules = "This is a place of positivity - Leave the negative vibes at the door!"
+    client.say("#rock_a_goth", rules);
+}
+
+function spookyTimer() {
+    let spooky = "Hey I'm Spooky. I ate nightbot... Well parts... It can do some commands but I'm better. Type in the word, commands to see what I can do! "
+    client.say("#rock_a_goth", spooky);
+}
+  
+function spookyIntervalFunc() {
+    let factIndex = Math.floor(Math.random() * (Data.length - 0 + 1) + 0)
+    let fact = Data[factIndex]
+    client.say("#rock_a_goth", fact);
+}
+
+
+// Setup connection configurations
+// These include the channel, username and password
+const client = new tmi.Client({
+    options: { debug: true, messagesLogLevel: "info" },
+    connection: {
+        reconnect: true,
+        secure: true
+    },
+
+    // Lack of the identity tags makes the bot anonymous and able to fetch messages from the channel
+    // for reading, supervision, spying, or viewing purposes only
+    identity: {
+        username: `${process.env.TWITCH_USERNAME}`,
+        password: `oauth:${process.env.TWITCH_OAUTH}`
+    },
+    channels: [`${process.env.TWITCH_CHANNEL}`]
+});
+
+// Connect to the channel specified using the setings found in the configurations
+// Any error found shall be logged out in the console
+client.connect().catch(console.error);
+
+//call timer functions
+setInterval(spookyIntervalFunc, 900000); //15 mins
+setInterval(followTimer, 18000000); //30
+setInterval(spookyTimer, 24000000); //40
+setInterval(tyTimer, 27000000); //45
+setInterval(rulesTimer, 36000000); //60
+
+
+
+// When the bot is on, it shall fetch the messages send by user from the specified channel
+client.on('message', (channel, tags, message, self) => {
+    // Lack of this statement or it's inverse (!self) will make it in active
+    if (self) return;
+
+    // Create up a switch statement with some possible commands and their outputs
+    // The input shall be converted to lowercase form first
+    // The outputs shall be in the chats
+    
+    switch (message.split(" ")[0].toLowerCase()) {
+        // Use 'tags' to obtain the username of the one who has keyed in a certain input
+        // 'channel' shall be used to specify the channel name in which the message is going to be displayed
+        //For one to send a message in a channel, you specify the channel name, then the message
+        // We shall use backticks when using tags to support template interpolation in JavaScript
+        
+        // In case the message in lowercase is equal to the string 'commands', send the sender of that message some of the common commands
+
+        case 'commands':
+            client.say(channel, `@${tags.username}, available commands are:
+            - commands - help - !lurk - !socials - !spooky - !slap user - !so user - !name - !spooky - For more info just type "help"
+            `);
+            break;
+            
+            // In case the message in lowercase is equal to the string '!website', send the sender of that message your personal website
+        case '!lurk':
+            client.say(channel, `@${tags.username}, has entered the shadowy realm of the LURK,  ever watching, ever present, but silent among the mists...  Thank you for the support and LURK ON!`);
+            break;
+
+        case '!socials':
+                client.say(channel, `visit my linktree for my socials! https://linktr.ee/rockagoth`);
+                break;
+            
+        //     // In case the message in lowercase is equal to the string 'greetings', send the sender of that message 'Hello @Username, what's up?!'
+        // case 'greetings':
+        //     client.say(channel, `Hello @${tags.username}, what's up?!`);
+        //     break;
+
+        // case '!sexybear':
+        //     client.say(channel, `@${tags.username}, Nina is the sexy bear!`);
+        //     break;
+
+        // case '!kitty':
+        //     client.say(channel, `LEILA BEAR!`);
+        //     break;
+
+        case '!spooky':
+            let factIndex = Math.floor(Math.random() * (Data.length - 0 + 1) + 0)
+            let fact = Data[factIndex]
+            client.say(channel, fact);
+            break;
+        
+            // In case the message in lowercase is equal to the string 'hi', send the sender of that message 'Username, hola'
+
+        case '!slap':
+            if (!vips.includes(tags.username)) {
+                client.say(channel, "Must be a Sub, VIP or Mod to slap someone")
+                break;
+            }
+            let slappedUser = message.split(" ")[1]
+            client.say(channel, `${slappedUser} just got slapped by ${tags.username}`);
+            break;
+
+        case '!so':
+            if (!vips.includes(tags.username)) {
+                client.say(channel, "Must be a VIP or Mod to give shoutout")
+                break;
+            }
+            let soUser = message.split(" ")[1]
+            client.say(channel, `Please check out and follow ${soUser} at Twitch.tv/${soUser}`);
+            break;
+            
+
+            // In case the message in lowercase is equal to the string '!name', send the sender of that message the name of the chatbot
+        case '!name':
+            client.say(channel, `Hello @${tags.username}, my name is SpookyBot! Boo!`);
+            break;
+            
+            // In case the message in lowercase is equal to the string 'help', send the sender of that message all the available help and commands
+        case 'help':
+            client.say(channel, `${tags.username}, Quick Help:
+
+            commands: Get Commands || 
+            help: Get Help || 
+            !spooky: Get random spooky fact || 
+            !lurk: Lurk Mode ||     }
+            !socials: My Linktree || 
+            !slap: Insert username after command - SUB, VIP, MOD only || 
+            !so: Give yourself a shoutout - SUB, VIP, MOD only ||
+            !name: Spookybot wants to say hi
+
+            `);
+            break;
+            
+            
+    //         // In case the message in lowercase is none of the above, check whether it is equal to '!upvote' or '!cheers'
+    //         // these are used to  like certain users' messages or celebrate them due to an achievement
+            
+    //     default:
+    //         // We shall convert the message into a string in which we shall check for its first word
+    //         // and use the others for output
+    //         let mymessage = message.toString();
+            
+    //         // We shall split the input message and check the string before the space if it is equal to '!upvote' or 'upvote'
+    //         if ((mymessage.split(' ')[0]).toLowerCase() === '!upvote' || 'upvote') {
+            
+    //             // You can add some emojis which will appear in the chat using their emoji names
+    //             // For example "PopCorn" or "TwitchLit" (fire emoji)
+    //             // We shall then take the first and second strings after the space and display them together with the username
+    //             // This shall output 'fireEmoji first_name second_name fireEmoji you have been UPVOTED by USERNAME'
+    //             client.say(channel, `TwitchLit @${(mymessage.split(' ')[1] + '_' + mymessage.split(' ')[2])} TwitchLit  you have been UPVOTED by ${ tags.username }`);
+
+
+    //             // We shall check if it is !cheer or cheers
+    //             // If so, we shall display beer emojis (HSCheers) and messages
+    //             // The bots output shall be 'beerEmoji first_name second_name beerEmoji you have been UPVOTED by USERNAME'
+    //         } else if ((mymessage.split(' ')[0]).toLowerCase() === '!cheer' || 'cheers') {
+    //             console.log(`HSCheers @${(mymessage.split(' ')[1] + '_' + mymessage.split(' ')[2])} HSCheers you have been UPVOTED by ${ tags.username }`);
+    //         }
+    //         break;
+    }
+});
---
+++ data.js
@@ -0,0 +1,96 @@
+let SpookyFacts = [
+    
+"Tooth-in-the-Eye Surgery. Surgeons put a tooth in a blind person’s eye to restore their sight. It was pioneered in the 1960s, and it actually works and it’s still being done today",
+
+"Real corpses were used in the 1982 film Poltergeist.",
+
+"There are bodies of over 150 dead hikers on Mount Everest, and they’are used as landmarks.",
+
+"The Turritopsis Dohrnii jellyfish is officially known as the only immortal creature in the world. It lives forever.",
+
+"A haunted radio station from Russia has been broadcasting a dull monotonous tone for twenty-four hours a day, seven days a week, for the last three-and-a-half decades. Every few seconds it’s joined by a second sound, like some ghostly ship sounding its foghorn.",
+
+
+"Climate change is making spiders bigger.",
+
+"Dogs like squeaky toys because they mimic the screams of their prey.",
+
+"Spiders can survive in space.",
+
+"Two Scottish surgeons originally invented the chainsaw to assist childbirth.",
+
+"Locked-In Syndrome is a condition in which a patient is fully aware but is stuck in a coma-like state.",
+
+"The film 'A Nightmare on Elm Street' is based on a real story reported in the LA Times. A boy was terrified to go to sleep, and when he did, he died while screaming about a nightmare.",
+
+"While Ted Bundy was a psychology major at the University of Washington, he worked at Seattle's Suicide Hotline Crisis Center.",
+
+"When you die the last sense to leave your body is the ability to hear.",
+
+"Ducklings can engage in cannibalistic behaviors when they're bored.",
+
+"More than 80% of our ocean is unmapped, unobserved, and unexplored.",
+
+"Technically it’s not impossible to die from holding in a sneeze. Some injuries from holding in a sneeze can be very serious, such as ruptured brain aneurysms, ruptured throat, and collapsed lungs.",
+
+"Horned Lizards can defend themselves by squirting blood out of their eyes.",
+
+"Alien hand syndrome is a phenomenon in which one hand is not under control of the mind. The person loses control of the hand, and it acts as if it has a mind of its own.",
+
+"Some ants turn into zombies via parasitic fungus which manipulates their brains.",
+
+"King Charles II drank alcohol mixed with pulverized human skulls.",
+
+"Arrhythmic death syndrome is a sudden condition where someone seemingly healthy dies suddenly with no apparent cause of death.", 
+
+"250,000 deaths a year are due to medical errors.",
+
+"During the mummification process, Ancient Egyptians remove the brain through one of the nostrils.",
+
+"People with Cotard's syndrome believe that parts of their body are missing, or that they are dying, dead, or don’t exist. They may think nothing exists.",
+
+"Criminologists estimate that there's a 1-in-3 chance police will never identify your killer if you're murdered in the US.",
+
+"Fatal familial insomnia makes it impossible for someone to sleep for months.",
+
+"You’re more likely to die on your birthday. The chance someone will die on their birthday is 6.7 percent, which is higher than any other day.",
+
+"Some female spiders allow their young to eat them alive.",
+
+"doctors have found a fir tree growing in a man’s lungs.",
+
+"A chicken named Mike lived for 8 weeks after his head was cut off.",
+
+"A group of crows is called a murder.",
+
+"Crows have also been known to seek revenge on people that have wronged them",
+
+"The Catacombs under Paris hold the rains of roughly sixty million plague victims.",
+
+"Ancient Britons often used human skulls as cups and bowls.",
+
+"In the Middle Ages, people used to eat mummies for medicinal purposes. It’s the main reason we do not have a lot of mummies left today.",
+
+"You share 80 million bacteria in a single kiss. ",
+
+"In the 1920’s, the American domestic terror group the Ku Klux Klan had a youth chapter called the “Ku Klux Kiddies.”",
+
+"A rat king is a bunch of rats that are tangled together by their tails. In a similiar situation is a squirell king.",
+
+"The former chief of the FBI estimates that there are between 25 and 50 active serial killers in America at any given time.",
+
+"The Pirates of The Caribbean ride at Disneyland used to have real skeletons as props.",
+
+"Syphilis left untreated leaves people looking like zombies. During the renaissance, hordes of untreated people with syphilis used to wander through the streets.",
+
+"In 2008 a man living in rural Japan noticed his food disappearing at night. When he installed cameras, they revealed a woman had been living in his house. At the time of her discovery, she had been living there for months.",
+
+"Dennis Radner, aka the BTK killer, profited off his own derangement. He got a lot of business installing security systems for people afraid of the BTK killer.",
+
+"A rat’s teeth never stop growing.",
+
+"Cosmologists theorize the constant expansion of the universe could cause it to tear apart. This theory is called The Big Rip."
+
+]
+
+module.exports = SpookyFacts;
