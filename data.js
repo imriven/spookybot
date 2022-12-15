853f3fcdf924125e62b16c9aa68ce24dbb2c81a3
Christina Harris <christinathedesigner@outlook.com>
2022-12-15 12:32:21 -0800

    spooky facts additions

--- data.js
+++ data.js
@@ -1,96 +1,113 @@
 let SpookyFacts = [
-    
-"Tooth-in-the-Eye Surgery. Surgeons put a tooth in a blind person’s eye to restore their sight. It was pioneered in the 1960s, and it actually works and it’s still being done today",
+  "Tooth-in-the-Eye Surgery. Surgeons put a tooth in a blind person’s eye to restore their sight. It was pioneered in the 1960s, and it actually works and it’s still being done today",
 
-"Real corpses were used in the 1982 film Poltergeist.",
+  "Real corpses were used in the 1982 film Poltergeist.",
 
-"There are bodies of over 150 dead hikers on Mount Everest, and they’are used as landmarks.",
+  "There are bodies of over 150 dead hikers on Mount Everest, and they’are used as landmarks.",
 
-"The git is officially known as the only immortal creature in the world. It lives forever.",
+  "The git is officially known as the only immortal creature in the world. It lives forever.",
 
-"A haunted radio station from Russia has been broadcasting a dull monotonous tone for twenty-four hours a day, seven days a week, for the last three-and-a-half decades. Every few seconds it’s joined by a second sound, like some ghostly ship sounding its foghorn.",
+  "A haunted radio station from Russia has been broadcasting a dull monotonous tone for twenty-four hours a day, seven days a week, for the last three-and-a-half decades. Every few seconds it’s joined by a second sound, like some ghostly ship sounding its foghorn.",
 
+  "Climate change is making spiders bigger.",
 
-"Climate change is making spiders bigger.",
+  "Dogs like squeaky toys because they mimic the screams of their prey.",
 
-"Dogs like squeaky toys because they mimic the screams of their prey.",
+  "Spiders can survive in space.",
 
-"Spiders can survive in space.",
+  "Two Scottish surgeons originally invented the chainsaw to assist childbirth.",
 
-"Two Scottish surgeons originally invented the chainsaw to assist childbirth.",
+  "Locked-In Syndrome is a condition in which a patient is fully aware but is stuck in a coma-like state.",
 
-"Locked-In Syndrome is a condition in which a patient is fully aware but is stuck in a coma-like state.",
+  "The film 'A Nightmare on Elm Street' is based on a real story reported in the LA Times. A boy was terrified to go to sleep, and when he did, he died while screaming about a nightmare.",
 
-"The film 'A Nightmare on Elm Street' is based on a real story reported in the LA Times. A boy was terrified to go to sleep, and when he did, he died while screaming about a nightmare.",
+  "While Ted Bundy was a psychology major at the University of Washington, he worked at Seattle's Suicide Hotline Crisis Center.",
 
-"While Ted Bundy was a psychology major at the University of Washington, he worked at Seattle's Suicide Hotline Crisis Center.",
+  "When you die the last sense to leave your body is the ability to hear.",
 
-"When you die the last sense to leave your body is the ability to hear.",
+  "Ducklings can engage in cannibalistic behaviors when they're bored.",
 
-"Ducklings can engage in cannibalistic behaviors when they're bored.",
+  "More than 80% of our ocean is unmapped, unobserved, and unexplored.",
 
-"More than 80% of our ocean is unmapped, unobserved, and unexplored.",
+  "Technically it’s not impossible to die from holding in a sneeze. Some injuries from holding in a sneeze can be very serious, such as ruptured brain aneurysms, ruptured throat, and collapsed lungs.",
 
-"Technically it’s not impossible to die from holding in a sneeze. Some injuries from holding in a sneeze can be very serious, such as ruptured brain aneurysms, ruptured throat, and collapsed lungs.",
+  "Horned Lizards can defend themselves by squirting blood out of their eyes.",
 
-"Horned Lizards can defend themselves by squirting blood out of their eyes.",
+  "Alien hand syndrome is a phenomenon in which one hand is not under control of the mind. The person loses control of the hand, and it acts as if it has a mind of its own.",
 
-"Alien hand syndrome is a phenomenon in which one hand is not under control of the mind. The person loses control of the hand, and it acts as if it has a mind of its own.",
+  "Some ants turn into zombies via parasitic fungus which manipulates their brains.",
 
-"Some ants turn into zombies via parasitic fungus which manipulates their brains.",
+  "King Charles II drank alcohol mixed with pulverized human skulls.",
 
-"King Charles II drank alcohol mixed with pulverized human skulls.",
+  "Arrhythmic death syndrome is a sudden condition where someone seemingly healthy dies suddenly with no apparent cause of death.",
 
-"Arrhythmic death syndrome is a sudden condition where someone seemingly healthy dies suddenly with no apparent cause of death.", 
+  "250,000 deaths a year are due to medical errors.",
 
-"250,000 deaths a year are due to medical errors.",
+  "During the mummification process, Ancient Egyptians remove the brain through one of the nostrils.",
 
-"During the mummification process, Ancient Egyptians remove the brain through one of the nostrils.",
+  "People with Cotard's syndrome believe that parts of their body are missing, or that they are dying, dead, or don’t exist. They may think nothing exists.",
 
-"People with Cotard's syndrome believe that parts of their body are missing, or that they are dying, dead, or don’t exist. They may think nothing exists.",
+  "Criminologists estimate that there's a 1-in-3 chance police will never identify your killer if you're murdered in the US.",
 
-"Criminologists estimate that there's a 1-in-3 chance police will never identify your killer if you're murdered in the US.",
+  "Fatal familial insomnia makes it impossible for someone to sleep for months.",
 
-"Fatal familial insomnia makes it impossible for someone to sleep for months.",
+  "You’re more likely to die on your birthday. The chance someone will die on their birthday is 6.7 percent, which is higher than any other day.",
 
-"You’re more likely to die on your birthday. The chance someone will die on their birthday is 6.7 percent, which is higher than any other day.",
+  "Some female spiders allow their young to eat them alive.",
 
-"Some female spiders allow their young to eat them alive.",
+  "doctors have found a fir tree growing in a man’s lungs.",
 
-"doctors have found a fir tree growing in a man’s lungs.",
+  "A chicken named Mike lived for 8 weeks after his head was cut off.",
 
-"A chicken named Mike lived for 8 weeks after his head was cut off.",
+  "A group of crows is called a murder.",
 
-"A group of crows is called a murder.",
+  "Crows have also been known to seek revenge on people that have wronged them",
 
-"Crows have also been known to seek revenge on people that have wronged them",
+  "The Catacombs under Paris hold the rains of roughly sixty million plague victims.",
 
-"The Catacombs under Paris hold the rains of roughly sixty million plague victims.",
+  "Ancient Britons often used human skulls as cups and bowls.",
 
-"Ancient Britons often used human skulls as cups and bowls.",
+  "In the Middle Ages, people used to eat mummies for medicinal purposes. It’s the main reason we do not have a lot of mummies left today.",
 
-"In the Middle Ages, people used to eat mummies for medicinal purposes. It’s the main reason we do not have a lot of mummies left today.",
+  "You share 80 million bacteria in a single kiss. ",
 
-"You share 80 million bacteria in a single kiss. ",
+  "In the 1920’s, the American domestic terror group the Ku Klux Klan had a youth chapter called the “Ku Klux Kiddies.”",
 
-"In the 1920’s, the American domestic terror group the Ku Klux Klan had a youth chapter called the “Ku Klux Kiddies.”",
+  "A rat king is a bunch of rats that are tangled together by their tails. In a similiar situation is a squirell king.",
 
-"A rat king is a bunch of rats that are tangled together by their tails. In a similiar situation is a squirell king.",
+  "The former chief of the FBI estimates that there are between 25 and 50 active serial killers in America at any given time.",
 
-"The former chief of the FBI estimates that there are between 25 and 50 active serial killers in America at any given time.",
+  "The Pirates of The Caribbean ride at Disneyland used to have real skeletons as props.",
 
-"The Pirates of The Caribbean ride at Disneyland used to have real skeletons as props.",
+  "Syphilis left untreated leaves people looking like zombies. During the renaissance, hordes of untreated people with syphilis used to wander through the streets.",
 
-"Syphilis left untreated leaves people looking like zombies. During the renaissance, hordes of untreated people with syphilis used to wander through the streets.",
+  "In 2008 a man living in rural Japan noticed his food disappearing at night. When he installed cameras, they revealed a woman had been living in his house. At the time of her discovery, she had been living there for months.",
 
-"In 2008 a man living in rural Japan noticed his food disappearing at night. When he installed cameras, they revealed a woman had been living in his house. At the time of her discovery, she had been living there for months.",
+  "Dennis Radner, aka the BTK killer, profited off his own derangement. He got a lot of business installing security systems for people afraid of the BTK killer.",
 
-"Dennis Radner, aka the BTK killer, profited off his own derangement. He got a lot of business installing security systems for people afraid of the BTK killer.",
+  "A rat’s teeth never stop growing.",
 
-"A rat’s teeth never stop growing.",
+  "Cosmologists theorize the constant expansion of the universe could cause it to tear apart. This theory is called The Big Rip.",
 
-"Cosmologists theorize the constant expansion of the universe could cause it to tear apart. This theory is called The Big Rip."
+  "The Voynich Manuscript is an ancient book full of strange symbols no one can translate. It's full of strange symbols, drawings and letters.",
 
-]
+  "In 1872 the ship Mary Celest was discovered floating in the ocean with no signs of its crew or passengers! The ship was in good condition with no signs of damage except a bit of water at the bottom. What happened to the passengers on board?",
+
+  "In 897AD, A dead Pope was once put on trial Pope Formosus was put on trial for perjury and other crimes. He'd been dead for a year! He was dug up and taken to court for questioning...",
+
+  "Jeremy Bentham, an 18th century philosopher, scholar and one of the founders of UCL, died in 1832. But that didn't stop him continuing to attend council meetings - his skeleton can now be found in the halls of UCL., dressed up in his clothes and stuffed with straw, and has been in attendance at UCL council meetings on at least one recorded occasion, in 2013. In the meeting notes he is recorded as 'Present, but not voting'.",
+
+  "Horned Lizards squirt blood from their eyes. This act is a way to freak out predators.",
+
+  "The Mummies of Guanajuato may have been buried alive. These mummies are famous for their horrible, dramatic facial expressions and date back to a 19th century Cholera epidemic.",
+
+  "Loch Morar is home to 'Morag', another lake monster who's been sighted many times over the years. Some people think there may even be underground tunnels between the two lochs, and that Nessie and Morag are one and the same!",
+
+  "The New England Vampire Panic occurred in the 19th century. These so called 'vampires' were actually suffering from tuberculosis.",
+
+  "Dracula is based on a historical figure, Vlad Tepes, or Vlad Dracula, who was a Romanian ruler in the 15th century who liked to stick his enemies on spikes. His nickname was Vlad the Impaler. The 'Dracula' in his original name means ' son of the dragon' (Dracul being his dad's nickname), and refers to his fearsome reputation.",
+
+  "Cockroaches can live without their heads for weeks but will ultimately die when they need food or water",
+];
 
 module.exports = SpookyFacts;
